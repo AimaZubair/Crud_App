078b1aa8fbdc6165e9fd3346e72b58038c736968
AimaZubar <Aimakh@users.noreply.github.com>
2021-07-02 13:45:51 +0500

    Splash screen and app icon and contact us

---
+++ final_crud_app/lib/components/editdata.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import '../Homepage.dart';
+import 'details.dart';
+
+import 'package:http/http.dart' as http;
+
+class EditData extends StatefulWidget {
+  final List list;
+  final int index;
+
+  EditData({@required this.list, @required this.index});
+
+  @override
+  _EditDataState createState() => _EditDataState();
+}
+
+class _EditDataState extends State<EditData> {
+  TextEditingController id;
+  TextEditingController name;
+  TextEditingController email;
+  TextEditingController mobile;
+  TextEditingController address;
+  void editData() {
+    var url = "http://192.168.1.100/dashboard/crud/editdata.php";
+
+    http.post(url, body: {
+      "id": widget.list[widget.index]['id'],
+      "name": name.text,
+      "mobile": mobile.text,
+      "email": email.text,
+      "address": address.text
+    });
+  }
+
+  @override
+  void initState() {
+    id = new TextEditingController(text: widget.list[widget.index]['id']);
+    name = new TextEditingController(text: widget.list[widget.index]['name']);
+    email = new TextEditingController(text: widget.list[widget.index]['email']);
+    mobile =
+        new TextEditingController(text: widget.list[widget.index]['mobile']);
+    address =
+        new TextEditingController(text: widget.list[widget.index]['address']);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: Text("Edit Data"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+            new Column(
+              children: <Widget>[
+                new TextField(
+                  controller: name,
+                  decoration: new InputDecoration(
+                    labelText: "Name",
+                  ),
+                ),
+                new TextField(
+                  controller: email,
+                  decoration: new InputDecoration(
+                    labelText: "Email",
+                  ),
+                ),
+                new TextField(
+                  controller: mobile,
+                  decoration: new InputDecoration(
+                    labelText: "Mobile",
+                  ),
+                ),
+                new TextField(
+                  controller: address,
+                  decoration: new InputDecoration(
+                    labelText: "Address",
+                  ),
+                ),
+                new Padding(padding: EdgeInsets.all(10.0)),
+                new RaisedButton(
+                  onPressed: () {
+                    editData();
+                    Navigator.of(context).push(
+                      new MaterialPageRoute(
+                          builder: (BuildContext context) => Home()),
+                    );
+                  },
+                  child: new Text(
+                    "Edit  Data",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  color: Colors.green,
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ final_crud_app/lib/contactus.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class contactus extends StatelessWidget {
+  static const String routeName = '/contact';
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contact Us'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top: 30.0),
+                child: Text(
+                  'Contact Us',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto-Italic',
+                    fontSize: 40.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 50.0),
+                child: Text(
+                  '033429019289',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto-Italic',
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 70.0),
+                child: Text(
+                  'aimak2340@gmail.com',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Roboto-Italic',
+                    fontSize: 20.0,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
